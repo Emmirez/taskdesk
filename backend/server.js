a18015008ac0a2042a548e0ccc37043e3cb47305
Emmirez Obaro <owhekoraobaro@example.com>
2026-04-15 04:41:13 -0400

    Update CLIENT_URL for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -16,11 +16,16 @@ connectDB();
 const app = express();
 
 //  Middleware
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://taskdesk-chi.vercel.app",
+];
+
 app.use(
   cors({
-    origin: true,
+    origin: allowedOrigins,
     credentials: true,
-  }),
+  })
 );
 app.use(express.json({ limit: "10kb" }));
 app.use(express.urlencoded({ extended: true }));
